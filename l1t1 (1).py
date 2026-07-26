c1ad9184b1bb755ff00f05d423ec998ec131ae0a
Vinay Sri <thontavinaysri@gmail.com>
2026-07-26 22:37:05 +0530

    Delete all files

---
+++ l1t1 (1).py
@@ -0,0 +1,16 @@
+student_name = "Vinay"     
+marks_obtained = 80.45          
+
+def display():
+    return "Python Function"
+
+class Student:
+    pass
+
+my_value = 100                  
+
+print("Variable:", student_name)
+print("Constant:", marks_obtained)
+print("Function:", display())
+print("Class:", Student)
+print("Underscore Variable:", my_value)
