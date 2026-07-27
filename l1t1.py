62202662ec3e6dd31cd04760265174f51457c4be
Vinay Sri <thontavinaysri@gmail.com>
2026-07-27 11:48:11 +0530

    files

---
+++ l1t1.py
@@ -0,0 +1,15 @@
+# Name :Thonta Vinaysri Mahesh Gowd
+#lab1,task1
+temperature = 28
+GRAVITY = 9.8
+def welcome():
+    return "physics class"
+class Book:
+    pass
+book_title = "some book"
+
+print("Temperature:", temperature)
+print("Gravity:", GRAVITY)
+print("Function:", welcome())
+print("Class:", Book.__name__)
+print("Book Title:", book_title)
