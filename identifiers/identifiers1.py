ffebfb1fb68f74d746b2d6d6379d0c6285c710b5
Vinay Sri <thontavinaysri@gmail.com>
2026-07-27 12:56:05 +0530

    initial commit

---
+++ identifiers/identifiers1.py
@@ -0,0 +1,21 @@
+temperature = 28
+GRAVITY = 9.8
+def welcome():
+    return "PHYSICS CLASS"
+class Book:
+    pass
+book_title = "SOME BOOK"
+
+print("Temperature:", temperature)
+print("Gravity:", GRAVITY)
+print("Function:", welcome())
+print("Class:", Book.__name__)
+print("Book Title:", book_title)
+
+'''Output:
+Temperature: 28
+Gravity: 9.8
+Function: PHYSICS CLASS
+Class: Book
+Book Title: SOME BOOK
+'''
---
+++ identifiers/identifiers3.py
@@ -0,0 +1,10 @@
+username = "mahesh"
+Username = "Vinay"
+
+print("username =", username)
+print("Username =", Username)
+
+'''Output:
+username = mahesh
+Username = Vinay
+'''
