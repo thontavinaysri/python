eb8a80cca6a857448d9f2cc0561bfb3a692f64e2
Vinay Sri <thontavinaysri@gmail.com>
2026-08-18 12:03:54 +0530

    initial commit

---
+++ lab3/exe.py
@@ -0,0 +1,24 @@
+#25341a05l1 vinay
+n=4
+for i in range(1,n+1):
+    if i==n:
+        print("*"*(2*n))
+    elif i==n-1:
+        print("*"*(i)+" "+"*"*i)
+    else:
+        print("*"*(i)+" "*(n+2-2*i)+"*" *i)
+for i in range(n-1,0,-1):
+    if i==n-1:
+        print("*"*(i)+" "+"*"*i)
+    else:
+        print("*"*(i)+" "*(n+2-2*i)+"*"*i)
+
+'''output 
+*    *
+**  **
+*** ***
+********
+*** ***
+**  **
+*    *
+'''
